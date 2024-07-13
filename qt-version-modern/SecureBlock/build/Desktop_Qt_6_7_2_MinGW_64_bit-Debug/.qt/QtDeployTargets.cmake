4746c4851e7fb6bfe1e5ef3f6c6ea5f289a0a55e
Claire Yurev <claireyureva@gmail.com>
2024-07-13 13:28:50 -0700

    qt 2

---
+++ qt-version-modern/SecureBlock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SecureBlock_FILE C:/Github/SecureBlock/Qt/SecureBlock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/SecureBlock.exe)
+set(__QT_DEPLOY_TARGET_SecureBlock_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SecureBlock_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ qt-version-modern/SecureBlock/main.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QSplashScreen>
+#include <QTimer>
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "SecureBlock_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            application.installTranslator(&translator);
+            break;
+        }
+    }
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap("C:/Github/SecureBlock/Qt/SecureBlock/splash.png"));
+    splash->show();
+
+    MainWindow w;
+    QTimer::singleShot(2000,splash,SLOT(close()));
+    QTimer::singleShot(2000,&w,SLOT(show()));
+    //w.show();
+    application.processEvents();
+    return application.exec();
+}
